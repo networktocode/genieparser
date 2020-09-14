aa429b15bf1a9603d2e26da86a30a9e088d030fe
Roy Eustaquio <45174296+reustaquio22@users.noreply.github.com>
2020-09-14 11:50:30 -0400

    Update test_folder_parsing.py

--- tests/test_folder_parsing.py
+++ tests/test_folder_parsing.py
@@ -82,7 +82,6 @@ CLASS_SKIP = {
         "ShowSoftwaretab": True, # PR submitted
         "ShowOmpSummary": True, # To be migrated
         "ShowSdwanOmpSummary": True, # To be migrated
-        "ShowSystemStatus": True, # To be migrated
         "ShowRebootHistory": True, # To be migrated
         "ShowSdwanRebootHistory": True, # To be migrated
         "ShowSslProxyStatistics": True, # To be migrated
@@ -97,7 +96,6 @@ CLASS_SKIP = {
         "ShowApphostingList": True, # To be migrated
         "ShowApRfProfileSummary": True, # To be migrated
         "ShowHwModuleStatus": True, # To be migrated
-        "ShowRunInterface": True, # To be migrated
         "ShowSdwanVersion": True, # To be migrated
         "ShowSdwanSoftware": True, # To be migrated
     },
